8f698dcaaca236284e6c1cb5472d288e4f8677dd
Ryan <eharris1127@gmail.com>
2016-11-04 19:12:29 -0400

    Trying to get bootstrap working; added direct tag for the moment

--- app.js
+++ app.js
@@ -44,7 +44,7 @@
 /* 0 */
 /***/ function(module, exports, __webpack_require__) {
 
-	(function webpackMissingModule() { throw new Error("Cannot find module \"./src/\""); }());
+	(function webpackMissingModule() { throw new Error("Cannot find module \"../src/js/app.js\""); }());
 
 
 /***/ }
